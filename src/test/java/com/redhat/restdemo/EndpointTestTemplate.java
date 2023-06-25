5c985764d5f29b438a94359e720e26080c09fe5d
Petr Hejcl <xhejcl@fi.muni.cz>
2023-06-25 15:50:47 +0200

    Add new author tests

---
+++ src/test/java/com/redhat/restdemo/EndpointTestTemplate.java
@@ -0,0 +1,57 @@
+package com.redhat.restdemo;
+
+import com.redhat.restdemo.model.repository.AuthorRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@RunWith(SpringRunner.class)
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EndpointTestTemplate {
+
+    @Value("http://localhost:${local.server.port}")
+    String baseUrl;
+
+    static int idCounter = 0;
+
+    TestRequests testRequests = new TestRequests();
+
+    @Container
+    private static PostgreSQLContainer postgresqlContainer;
+
+    static {
+        postgresqlContainer = new PostgreSQLContainer("postgres:14")
+                .withDatabaseName("postgres")
+                .withUsername("compose-postgres")
+                .withPassword("compose-postgres");
+        postgresqlContainer.start();
+    }
+
+    @DynamicPropertySource
+    protected static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
+    protected String createURLWithPort(String uri) {
+        return baseUrl + uri;
+    }
+
+    protected  <T> ResponseEntity<String> postAndIncrease(String url, T object) {
+        ResponseEntity<String> response = testRequests.post(url, object);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            idCounter++;
+        }
+        return response;
+    }
+}
--- src/test/java/com/redhat/restdemo/utils/Utils.java
+++ src/test/java/com/redhat/restdemo/utils/Utils.java
@@ -1,10 +1,6 @@
 package com.redhat.restdemo.utils;
 
-import com.redhat.restdemo.model.entity.Author;
-
-import java.util.Objects;
-
-public class utils {
+public class Utils {
     public static <T> Long countGetResult(Iterable<T> objects) {
         Long counter = 0L;
         for (Object object: objects) {
