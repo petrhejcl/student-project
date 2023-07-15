454cfb136b89a086ef6f1cc39de0da04d029244f
Petr Hejcl <xhejcl@fi.muni.cz>
2023-07-15 19:54:11 +0200

    update logger messages

--- src/main/java/com/redhat/restdemo/model/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.restdemo.model.repository;
-
-import com.redhat.restdemo.model.entity.Genre;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends CrudRepository<Genre, String> {
-}
