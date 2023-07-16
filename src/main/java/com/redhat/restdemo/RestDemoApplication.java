d0296c4a9bb3703e03ba8e0b698afe78feea5324
Nextap <nextap@Nextaps-MacBook-Pro.local>
2023-07-17 00:03:19 +0200

    update book tests

--- src/main/java/com/redhat/restdemo/RestDemoApplication.java
+++ src/main/java/com/redhat/restdemo/RestDemoApplication.java
@@ -1,28 +1,61 @@
 package com.redhat.restdemo;
 
-import com.redhat.restdemo.model.entity.Author;
-import com.redhat.restdemo.model.entity.Book;
+import com.redhat.restdemo.model.entity.*;
+import com.redhat.restdemo.model.repository.*;
 import com.redhat.restdemo.utils.TestRequests;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.filter.OrderedWebFilter;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestDemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RestDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(RestDemoApplication.class, args);
+
+		BookRepository bookRepository = context.getBean(BookRepository.class);
+		LibraryRepository libraryRepository = context.getBean(LibraryRepository.class);
+		AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
+		AuthorshipRepository authorshipRepository = context.getBean(AuthorshipRepository.class);
+		OwnershipRepository ownershipRepository = context.getBean(OwnershipRepository.class);
 
 		Author charles = new Author("Charles", "Bukowski", 1920);
 		Author mark = new Author("Mark", "Twain", 1835);
 		Author frank = new Author("Frank", "Herbert", 1920);
 
+		authorRepository.save(charles);
+		authorRepository.save(mark);
+		authorRepository.save(frank);
+
 		Book dune = new Book(9780441172719L, "Dune", 1965, "Sci-Fi");
 		Book postOffice = new Book(9780061177576L, "Post Office", 1971, "Novel");
 
+		bookRepository.save(dune);
+		bookRepository.save(postOffice);
+
+		Library brno = new Library("Brnenska knihovna", "Brno", "Sumavska", 111, "Skvela knihhovna");
+		Library praha = new Library("Prazska knihovna", "Praha", "Vodickova", 222, "Skvela knihhovna v Praze");
+
+		libraryRepository.save(brno);
+		libraryRepository.save(praha);
+
+		authorshipRepository.save(new Authorship(postOffice.getId(), charles.getId()));
+		authorshipRepository.save(new Authorship(dune.getId(), mark.getId()));
+
+		ownershipRepository.save(new Ownership(brno.getId(), postOffice.getId()));
+		ownershipRepository.save(new Ownership(brno.getId(), dune.getId()));
+		ownershipRepository.save(new Ownership(praha.getId(), postOffice.getId()));
+
+		/*
 		String url = "http://localhost:8080";
 		String author = "/author";
 		String books = "/book";
+		String library = "/library";
 		String postAuthorUri = author + "/add";
 		String postBookUri = books + "/add";
+		String postLibrary = library + "/add";
+
 
 		TestRequests testRequests = new TestRequests();
 
@@ -32,5 +65,6 @@ public class RestDemoApplication {
 
 		testRequests.post(url + postBookUri, dune);
 		testRequests.post(url + postBookUri, postOffice);
+		*/
 	}
 }
--- src/test/java/com/redhat/restdemo/OwnershipEndpointTests.java
+++ src/test/java/com/redhat/restdemo/OwnershipEndpointTests.java
@@ -4,8 +4,6 @@ package com.redhat.restdemo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.redhat.restdemo.model.entity.Author;
-import com.redhat.restdemo.model.entity.Authorship;
 import com.redhat.restdemo.model.entity.Book;
 import com.redhat.restdemo.model.entity.Library;
 import com.redhat.restdemo.model.entity.Ownership;
@@ -13,8 +11,6 @@ import com.redhat.restdemo.model.repository.BookRepository;
 import com.redhat.restdemo.model.repository.LibraryRepository;
 import com.redhat.restdemo.model.repository.OwnershipRepository;
 import com.redhat.restdemo.utils.TestData;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +19,9 @@ import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
+import static com.redhat.restdemo.utils.Utils.countIterable;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -39,14 +37,18 @@ class OwnershipEndpointTests extends EndpointTestTemplate {
 
     private List<Ownership> ownerships;
 
-    private void prepareLibraryBookSchemas() {
+    private void prepareOwnerships() {
         for (Map.Entry<Book, Library> entry : TestData.ownership.entrySet()) {
             Integer bookId = bookRepository.save(entry.getKey()).getId();
             Integer libraryId = libraryRepository.save(entry.getValue()).getId();
             Ownership ownership = new Ownership(bookId, libraryId);
-            ownershipRepository.save(ownership);
             ownerships.add(ownership);
         }
+    }
+
+    private void prepareOwnershipsSchema() {
+        prepareOwnerships();
+        ownershipRepository.saveAll(ownerships);
     }
 
     @BeforeEach
@@ -56,12 +58,10 @@ class OwnershipEndpointTests extends EndpointTestTemplate {
     }
 
     @Test
-    void testGetOwnershipsEndpoint() throws JsonProcessingException {
-        prepareLibraryBookSchemas();
+    void testGetAllOwnershipsEndpoint() throws JsonProcessingException {
+        prepareOwnershipsSchema();
 
-        String ownershipUrl = createURLWithPort("/ownership");
-
-        ResponseEntity<String> response = testRequests.get(ownershipUrl);
+        ResponseEntity<String> response = testRequests.get(createURLWithPort("/ownership"));
 
         ObjectMapper objectMapper = new ObjectMapper();
         List<Ownership> bookOwnership = objectMapper.readValue(response.getBody(), new TypeReference<>() {
@@ -74,4 +74,57 @@ class OwnershipEndpointTests extends EndpointTestTemplate {
         }
     }
 
+    @Test
+    void testAddOwnershipEndpoint() throws JsonProcessingException {
+        prepareOwnerships();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String addOwnershipUrl = createURLWithPort("/ownership/add");
+
+        Iterable<Ownership> allOwnerships = ownershipRepository.findAll();
+        long allOwnershipsSize = countIterable(allOwnerships);
+        assertThat(allOwnershipsSize, is(0L));
+
+        for (Ownership ownership : ownerships) {
+            ResponseEntity<String> response = testRequests.post(addOwnershipUrl, ownership);
+            assertThat(response.getStatusCode().is2xxSuccessful(), is(true));
+            assertThat(countIterable(ownershipRepository.findAll()), is(allOwnershipsSize + 1));
+            allOwnershipsSize += 1;
+            Ownership newOwnership = objectMapper.readValue(response.getBody(), new TypeReference<>() {
+            });
+            ownership.setId(newOwnership.getId());
+        }
+
+        for (Ownership ownership : ownershipRepository.findAll()) {
+            assertThat(ownerships.contains(ownership), is(true));
+        }
+    }
+
+    @Test
+    void testDeleteOwnershipEndpoint() {
+        prepareOwnershipsSchema();
+
+        String deleteOwnershipUrl = createURLWithPort("/ownership/delete");
+
+        Iterable<Ownership> allOwnerships = ownershipRepository.findAll();
+        long allOwnershipSize = countIterable(ownerships);
+
+        for (int i = 0; i < 3; i++) {
+            int nonSenseId = new Random().nextInt(50000) + 100;
+            ResponseEntity<String> nonSenseResponse = testRequests.delete(deleteOwnershipUrl + "/" + nonSenseId);
+            assertThat(nonSenseResponse.getStatusCode().is4xxClientError(), is(true));
+            assertThat(countIterable(ownershipRepository.findAll()), is(allOwnershipSize));
+        }
+
+        for (Ownership ownership : allOwnerships) {
+            Integer ownershipId = ownership.getId();
+            ResponseEntity<String> response = testRequests.delete(deleteOwnershipUrl + "/" + ownershipId);
+            allOwnershipSize -= 1;
+
+            assertThat(response.getStatusCode().is2xxSuccessful(), is(true));
+            assertThat(countIterable(ownershipRepository.findAll()), is(allOwnershipSize));
+            assertThat(ownershipRepository.existsById(ownership.getId()), is(false));
+        }
+    }
 }
