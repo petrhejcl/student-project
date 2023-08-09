eeea594faa0f72607799e0918310cb5ef2058d96
Nextap <nextap@Nextaps-MacBook-Pro.local>
2023-08-09 23:25:09 +0200

    add first authorService test

--- src/test/java/com/redhat/restdemo/unittests/AuthorServiceTest.java
+++ src/test/java/com/redhat/restdemo/unittests/AuthorServiceTest.java
@@ -1,23 +1,52 @@
 package com.redhat.restdemo.unittests;
 
+import com.redhat.restdemo.model.entity.Author;
+import com.redhat.restdemo.model.repository.AuthorRepository;
+import com.redhat.restdemo.model.repository.AuthorshipRepository;
+import com.redhat.restdemo.model.repository.BookRepository;
 import com.redhat.restdemo.model.service.AuthorService;
+import com.redhat.restdemo.model.service.AuthorServiceImpl;
+import com.redhat.restdemo.utils.TestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.mock;
+import static com.redhat.restdemo.utils.TestUtils.countIterable;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 class AuthorServiceTest {
 
-    private AuthorService authorService;
+    @InjectMocks
+    private AuthorService authorService = new AuthorServiceImpl();
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @Mock
+    private AuthorshipRepository authorshipRepository;
 
     @BeforeEach
-    void setUp() {
-        authorService = mock(AuthorService.class);
+    void setup() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     void findAll() {
+        when(authorRepository.findAll()).thenReturn(TestData.authors);
 
+        Iterable<Author> authors = authorService.findAll();
+
+        assertThat(countIterable(authors), is((long) TestData.authors.size()));
+
+        for (Author author : authors) {
+            assertTrue(TestData.authors.contains(author));
+        }
     }
-
 }
