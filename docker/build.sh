2906151766b3b347a474d22899f07fe0ba62d21a
Nextap <nextap@Nextaps-MacBook-Pro.local>
2023-09-09 21:10:06 +0200

    just testing gh actions

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# To build application correctly launch this script from parent directory as ./docker/build.sh
+# To build application correctly
+# launch this script from parent directory as ./docker/build.sh
 mvn clean install && cp target/*.jar docker/app && cd docker && docker compose down && docker compose build && docker compose up
